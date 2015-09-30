d94a6d8dd3b3f30812b926cf70d162105926ef59
jerlinjames <jerlinjames@hotmail.com>
2015-09-30 15:02:13 +0800

    FacebookAuthentication

---
+++ FacebookAuthenticationAngularJS/FacebookAuthenticationAngularJS/App/Controllers/HomeController.js
@@ -0,0 +1,34 @@
+﻿myApp.controller('HomeController', ['$scope', 'authFact', '$location', '$cookies', function ($scope, authfact, $location, $cookies) {
+    $scope.name = 'Login Please';
+
+    $scope.FBLogin = function() {
+        FB.login(function (response) {
+            if (response.authResponse) {
+                console.log('Welcome!  Fetching your information.... ');
+                FB.api('/me', function (response) {
+                    console.log('Good to see you, ' + response.name + '.');
+                    var userObjString = JSON.stringify(response);
+                    console.log(userObjString);
+
+                    $cookies.put('userObj', userObjString);
+                    
+                    //console.dir(JSON.parse($cookies.get('userObj')));
+                    
+
+                    var accesstoken = FB.getAuthResponse().accessToken;
+
+                    console.log(accesstoken);
+                    
+                    authfact.setAccessToken(accesstoken);
+
+                    $location.path('/dashboard');
+                    $scope.$apply();
+                });
+            } else {
+                console.log('User cancelled login or did not fully authorize.');
+            }
+        });
+    };
+
+
+}]);
